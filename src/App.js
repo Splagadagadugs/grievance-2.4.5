ec6197c6c29c7f3ee57a2c7a047236ce744e4dd0
kert <kmarquez1234546@gmail.com>
2022-12-15 16:59:09 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
+import Footer from './components/pages/Footer/Footer';
+import GrievanceForm from './components/pages/form/GrievanceForm';
+import Dashboard from './components/pages/AdminDashboard/Dashboard';
+import MainPage from './components/pages/AdminLogin/MainPage';
+import { AuthContextProvider } from './components/context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import PrivacyPolicy from './components/pages/Legal/PrivacyPolicy';
+import Terms from './components/pages/Legal/Terms';
+import Copyright from './components/pages/Legal/Copyright';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Navbar/>
+     <AuthContextProvider>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/form' element ={<GrievanceForm/>}/>
+      <Route path ='/dashboard' element = {<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+      <Route path ='/login' element = {<MainPage/>}/>
+      <Route path ='/privacy-policy' element = {<PrivacyPolicy/>}/>
+      <Route path ='/terms-and-conditions' element = {<Terms/>}/>
+      <Route path ='/copyright-notice' element = {<Copyright/>}/>
+     </Routes>
+     <Footer/>
+     </AuthContextProvider>
+      
+     
+    </Router>
   );
 }
 
